b11697c49cd8326209f171ff3ca773636070e185
rahul <samueljackson649@gmail.com>
2025-11-12 19:20:37 +0545

    just barely touched the auth system

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,6 @@
+import 'package:chat_app/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<UserEntity> loginUser(String username, String password);
+  Future<void> registerUser(String username, String password);
+}
